b5212b43aefb16081c109b423a7802b6d93f107e
Omar Ahmed <73648173+Omar2454@users.noreply.github.com>
2024-03-15 01:10:31 +0200

    update

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -4,22 +4,24 @@
     
     The size of the square must be the first argument 
     of the program.
-
-    change in size = parseInt(process.argv[2], 16)
 */
-
 
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+    process.exit(1);
 }
 
-size = parseInt(process.argv[2], 10)
+let size = parseInt(process.argv[2], 10);
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+if (isNaN(size)) {
+    process.stderr.write("Invalid number\n");
+    process.exit(1);
+}
+
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
